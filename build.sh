f4f7ac24b3b807510829586264cfce1e3c38320f
FlorinAndrei <901867+FlorinAndrei@users.noreply.github.com>
2025-02-27 10:38:42 -0800

    only try to delete images that exist

--- build.sh
+++ build.sh
@@ -8,4 +8,6 @@ docker build --progress=plain -t florinandrei/jupyter-server:$tag .
 docker tag florinandrei/jupyter-server:$tag florinandrei/jupyter-server:latest
 
 # delete older tags
-docker rmi $(docker images --format "{{.Repository}}:{{.Tag}}" | grep "florinandrei/jupyter-server" | grep -v $tag | grep -v latest)
+for img in $(docker images --format "{{.Repository}}:{{.Tag}}" | grep "florinandrei/jupyter-server" | grep -v $tag | grep -v latest); do
+  docker rmi $img
+done
